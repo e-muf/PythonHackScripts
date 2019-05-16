5d9f0986ab495f1ed2e57d7bbe2fb1c1af9d212b
root <root@localhost.localdomain>
2019-05-16 04:16:59 -0400

    Initialize Mac Changer project

---
+++ mac_changer/mac_changer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import subprocess
+import optparse
+
+parser = optparse.OptionParser()
+
+parser.add_option('-i', '--interface', dest='interface', help='Interface to change it\'s MAC address')
+parser.add_option('-m', '--mac', dest='new_mac', help='New MAC address')
+
+(options, arguments) = parser.parse_args()
+
+interface = options.interface
+new_mac = options.new_mac
+
+print("[+] Changing MAC address for " + interface + " to " + new_mac)
+
+subprocess.call(['ifconfig', interface, 'down'])
+subprocess.call(['ifconfig', interface, 'hw', 'ether', new_mac])
+subprocess.call(['ifconfig', interface, 'up'])
